28d2eefa31be6b0e8f6767d1082a280f73f9f0f0
lakshmi7977 <lakshmi7977@localhost>
2014-09-29 09:45:30 -0400

    Features: 	- Composable Period: Calendar, Coupon/Accrual DC, Coupon/Accrual EOM Adjustment 	- Composable Period: Accrual Start/End, Notional, Full Coupon DCF, Constructor 	- Composable Period: Accessors, Date Node Relative Location, Accrual DCF, Accrued01, NODE LOCATION Flags 	- Composable Period: Full Coupon 01, Accrual/Full Coupon Amounts, Composable Fixed/Float Period Accessor Trim out 	- Composable Fixed Period: Inheritance from Coupon Period, Constructor Delegation, Accrued/Full Coupon Accrued, Accessors 	- Composable Float Period: Inheritance from Coupon Period, Constructor Delegation, Accrued/Full Coupon Accrued, Accessors 	- Composable Fixed/Float Period: Fixed Coupon/Basis, FRI/Spread, Reference Rate 	- Composed Period: Frequency, Accrual Compounding Rule, Pay Date/Currency, FX Fixing Date 	- Composed Period: Base Notional, Notional Schedule, Credit Label, Composable Period List, Constructor 	- Composed Period: Period Appendage, Containment Check, FX MTM Check, Period FX Extraction, Period Tenor Calculation
    Bug Fixes:
    
    Samples:

---
+++ CreditAnalytics/dev/src/org/drip/analytics/cashflow/ComposedPeriod.java
@@ -0,0 +1,388 @@
+
+package org.drip.analytics.cashflow;
+
+/*
+ * -*- mode: java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ */
+
+/*!
+ * Copyright (C) 2014 Lakshmi Krishnamurthy
+ * 
+ *  This file is part of DRIP, a free-software/open-source library for fixed income analysts and developers -
+ * 		http://www.credit-trader.org/Begin.html
+ * 
+ *  DRIP is a free, full featured, fixed income rates, credit, and FX analytics library with a focus towards
+ *  	pricing/valuation, risk, and market making.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *   	you may not use this file except in compliance with the License.
+ *   
+ *  You may obtain a copy of the License at
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  	distributed under the License is distributed on an "AS IS" BASIS,
+ *  	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  
+ *  See the License for the specific language governing permissions and
+ *  	limitations under the License.
+ */
+
+/**
+ * ComposedPeriod implements the composed coupon period functionality.
+ *
+ * @author Lakshmi Krishnamurthy
+ */
+
+public abstract class ComposedPeriod {
+	private int _iFreq = 2;
+	private int _iAccrualCompoundingRule = -1;
+	private java.lang.String _strPayCurrency = "";
+	private double _dblPayDate = java.lang.Double.NaN;
+	private double _dblBaseNotional = java.lang.Double.NaN;
+	private double _dblFXFixingDate = java.lang.Double.NaN;
+	private org.drip.state.identifier.CreditLabel _creditLabel = null;
+	private org.drip.product.params.FactorSchedule _notlSchedule = null;
+	private java.util.List<org.drip.analytics.cashflow.ComposablePeriod> _lsComposablePeriod = null;
+
+	/**
+	 * ComposedPeriod Constructor
+	 * 
+	 * @param iFreq Frequency
+	 * @param dblPayDate Period Pay Date
+	 * @param strPayCurrency Pay Currency
+	 * @param iAccrualCompoundingRule The Accrual Compounding Rule
+	 * @param dblBaseNotional Coupon Period Base Notional
+	 * @param notlSchedule Coupon Period Notional Schedule
+	 * @param creditLabel The Credit Label
+	 * @param dblFXFixingDate The FX Fixing Date for non-MTM'ed Cash-flow
+	 * 
+	 * @throws java.lang.Exception Thrown if the Accrual Compounding Rule is invalid
+	 */
+
+	public ComposedPeriod (
+		final int iFreq,
+		final double dblPayDate,
+		final java.lang.String strPayCurrency,
+		final int iAccrualCompoundingRule,
+		final double dblBaseNotional,
+		final org.drip.product.params.FactorSchedule notlSchedule,
+		final org.drip.state.identifier.CreditLabel creditLabel,
+		final double dblFXFixingDate)
+		throws java.lang.Exception
+	{
+		if (0 >= (_iFreq = iFreq) || !org.drip.quant.common.NumberUtil.IsValid (_dblPayDate = dblPayDate) ||
+			null == (_strPayCurrency = strPayCurrency) || _strPayCurrency.isEmpty() ||
+				!org.drip.analytics.support.ResetUtil.ValidateCompoundingRule (_iAccrualCompoundingRule =
+					iAccrualCompoundingRule) || !org.drip.quant.common.NumberUtil.IsValid (_dblBaseNotional =
+						dblBaseNotional))
+			throw new java.lang.Exception ("ComposedPeriod ctr: Invalid Inputs");
+
+		_creditLabel = creditLabel;
+		_dblFXFixingDate = dblFXFixingDate;
+
+		if (null == (_notlSchedule = notlSchedule))
+			_notlSchedule = org.drip.product.params.FactorSchedule.CreateBulletSchedule();
+	}
+
+	/**
+	 * Append the specified Composable Period
+	 * 
+	 * @param cp The Composable Period
+	 * 
+	 * @return TRUE => The Composable Period Successfully Appended
+	 */
+
+	public boolean appendPeriod (
+		final org.drip.analytics.cashflow.ComposablePeriod cp)
+	{
+		if (null == cp) return false;
+
+		if (null == _lsComposablePeriod)
+			_lsComposablePeriod = new
+				java.util.ArrayList<org.drip.analytics.cashflow.ComposablePeriod>();
+
+		_lsComposablePeriod.add (cp);
+
+		return true;
+	}
+
+	/**
+	 * Retrieve the List of Composable Periods
+	 * 
+	 * @return The List of Composable Periods
+	 */
+
+	public java.util.List<org.drip.analytics.cashflow.ComposablePeriod> periods()
+	{
+		return _lsComposablePeriod;
+	}
+
+	/**
+	 * Check whether the supplied date is inside the period specified
+	 * 
+	 * @param dblDate Date input
+	 * 
+	 * @return True indicates the specified date is inside the period
+	 * 
+	 * @throws java.lang.Exception Thrown if input is invalid
+	 */
+
+	public boolean contains (
+		final double dblDate)
+		throws java.lang.Exception
+	{
+		if (!org.drip.quant.common.NumberUtil.IsValid (dblDate))
+			throw new java.lang.Exception ("ComposedPeriod::contains => Invalid Inputs");
+
+		double dblStartDate = _lsComposablePeriod.get (0).accrualStartDate();
+
+		if (dblStartDate > dblDate) return false;
+
+		double dblEndDate = _lsComposablePeriod.get (_lsComposablePeriod.size() - 1).accrualEndDate();
+
+		if (dblEndDate < dblDate) return false;
+
+		return true;
+	}
+
+	/**
+	 * Retrieve the Accrual Compounding Rule
+	 * 
+	 * @return The Accrual Compounding Rule
+	 */
+
+	public int accrualCompoundingRule()
+	{
+		return _iAccrualCompoundingRule;
+	}
+
+	/**
+	 * Return the period Pay Date
+	 * 
+	 * @return Period Pay Date
+	 */
+
+	public double payDate()
+	{
+		return _dblPayDate;
+	}
+
+	/**
+	 * Return the period FX Fixing Date
+	 * 
+	 * @return Period FX Fixing Date
+	 */
+
+	public double fxFixingDate()
+	{
+		return _dblFXFixingDate;
+	}
+
+	/**
+	 * Is this Cash Flow FX MTM'ed?
+	 * 
+	 * @return TRUE => FX MTM is on (i.e., FX is not driven by fixing)
+	 */
+
+	public boolean isFXMTM()
+	{
+		return !org.drip.quant.common.NumberUtil.IsValid (_dblFXFixingDate);
+	}
+
+	/**
+	 * Coupon Period FX
+	 * 
+	 * @param csqs Market Parameters
+	 * 
+	 * @return The Period FX
+	 * 
+	 * @throws java.lang.Exception Thrown if the Inputs are Invalid
+	 */
+
+	public double fx (
+		final org.drip.param.market.CurveSurfaceQuoteSet csqs)
+		throws java.lang.Exception
+	{
+		org.drip.state.identifier.FXLabel fxLabel = fxLabel();
+
+		if (null == fxLabel) return 1.;
+
+		if (null == csqs) throw new java.lang.Exception ("ComposedPeriod::fx => Invalid Inputs");
+
+		if (!isFXMTM()) return csqs.getFixing (_dblFXFixingDate, fxLabel);
+
+		org.drip.quant.function1D.AbstractUnivariate auFX = csqs.fxCurve (fxLabel);
+
+		if (null == auFX)
+			throw new java.lang.Exception ("ComposedPeriod::fx => No Curve for " +
+				fxLabel.fullyQualifiedName());
+
+		return auFX.evaluate (_dblPayDate);
+	}
+
+	/**
+	 * Retrieve the Coupon Frequency
+	 * 
+	 * @return The Coupon Frequency
+	 */
+
+	public int freq()
+	{
+		return _iFreq;
+	}
+
+	/**
+	 * Convert the Coupon Frequency into a Tenor
+	 * 
+	 * @return The Coupon Frequency converted into a Tenor
+	 */
+
+	public java.lang.String tenor()
+	{
+		int iTenorInMonths = 12 / _iFreq ;
+
+		return 1 == iTenorInMonths || 2 == iTenorInMonths || 3 == iTenorInMonths || 6 == iTenorInMonths || 12
+			== iTenorInMonths ? iTenorInMonths + "M" : "ON";
+	}
+
+	/**
+	 * Retrieve the Pay Currency
+	 * 
+	 * @return The Pay Currency
+	 */
+
+	public java.lang.String payCurrency()
+	{
+		return _strPayCurrency;
+	}
+
+	/**
+	 * Get the Period Base Notional
+	 * 
+	 * @return Period Base Notional
+	 */
+
+	public double baseNotional()
+	{
+		return _dblBaseNotional;
+	}
+
+	/**
+	 * Get the period Notional Schedule
+	 * 
+	 * @return Period Notional Schedule
+	 */
+
+	public org.drip.product.params.FactorSchedule notionalSchedule()
+	{
+		return _notlSchedule;
+	}
+
+	/**
+	 * Coupon Period Notional Corresponding to the specified Date
+	 * 
+	 * @param dblDate The Specified Date
+	 * 
+	 * @return The Period Notional Corresponding to the specified Date
+	 * 
+	 * @throws java.lang.Exception Thrown if the Inputs are Invalid
+	 */
+
+	public double notional (
+		final double dblDate)
+		throws java.lang.Exception
+	{
+		if (!org.drip.quant.common.NumberUtil.IsValid (dblDate) || !contains (dblDate))
+			throw new java.lang.Exception ("ComposedPeriod::notional => Invalid Inputs");
+
+		return _dblBaseNotional * (null == _notlSchedule ? 1. : _notlSchedule.getFactor (dblDate));
+	}
+
+	/**
+	 * Coupon Period Notional Aggregated over the specified Dates
+	 * 
+	 * @param dblDate1 The Date #1
+	 * @param dblDate2 The Date #2
+	 * 
+	 * @return The Period Notional Aggregated over the specified Dates
+	 * 
+	 * @throws java.lang.Exception Thrown if the Inputs are Invalid
+	 */
+
+	public double notional (
+		final double dblDate1,
+		final double dblDate2)
+		throws java.lang.Exception
+	{
+		if (!org.drip.quant.common.NumberUtil.IsValid (dblDate1) || !org.drip.quant.common.NumberUtil.IsValid
+			(dblDate2) || !contains (dblDate1) || !contains (dblDate2))
+			throw new java.lang.Exception ("ComposedPeriod::notional => Invalid Dates");
+
+		return _dblBaseNotional * (null == _notlSchedule ? 1. : _notlSchedule.getFactor (dblDate1,
+			dblDate2));
+	}
+
+	/**
+	 * Return the Collateral Label
+	 * 
+	 * @return The Collateral Label
+	 */
+
+	public org.drip.state.identifier.CollateralLabel collateralLabel()
+	{
+		return org.drip.state.identifier.CollateralLabel.Standard (_strPayCurrency);
+	}
+
+	/**
+	 * Return the Credit Label
+	 * 
+	 * @return The Credit Label
+	 */
+
+	public org.drip.state.identifier.CreditLabel creditLabel()
+	{
+		return _creditLabel;
+	}
+
+	/**
+	 * Return the Funding Label
+	 * 
+	 * @return The Funding Label
+	 */
+
+	public org.drip.state.identifier.FundingLabel fundingLabel()
+	{
+		return org.drip.state.identifier.FundingLabel.Standard (_strPayCurrency);
+	}
+
+	/**
+	 * Return the FX Label
+	 * 
+	 * @return The FX Label
+	 */
+
+	public org.drip.state.identifier.FXLabel fxLabel()
+	{
+		java.lang.String strCouponCurrency = couponCurrency();
+
+		return _strPayCurrency.equalsIgnoreCase (strCouponCurrency) ? null :
+			org.drip.state.identifier.FXLabel.Standard (_strPayCurrency + "/" + strCouponCurrency);
+	}
+
+	/**
+	 * Retrieve the Coupon Currency
+	 * 
+	 * @return The Coupon Currency
+	 */
+
+	abstract public java.lang.String couponCurrency();
+
+	/**
+	 * Return the Forward Label
+	 * 
+	 * @return The Forward Label
+	 */
+
+	abstract public org.drip.state.identifier.ForwardLabel forwardLabel();
+}
